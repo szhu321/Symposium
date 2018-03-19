5d6d7490151fe981a4c27ca8d0e5f2d15ddb4ece
Sheng <Sheng@Shengs-PC>
2018-03-19 19:28:44 -0400

    FinishRotation

--- BubbleShot/src/mainGame/RotationTest.java
+++ BubbleShot/src/mainGame/RotationTest.java
@@ -2,26 +2,54 @@ package mainGame;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
+import map.Room;
+import map.Tile.Tile;
 
 public class RotationTest extends Application
 {
 	private Stage window;
 	private Scene scene;
+	private Group moveArea;
+	private GridPane mapGrid;
+	private BorderPane headsUpDis;
 	private Group root;
-	private Rectangle rec;
-	private double angle;
+	
+	private Boolean up = false;
+	private Boolean down = false;
+	private Boolean left = false;
+	private Boolean right = false;
+	private Boolean shift = false;
+	private double mouseAngle = 0.0;
+	
+	private Shape player;
+	private double playerX = 0.0;
+	private double playerY = 0.0;
+	private double playerCenterX = 0.0;
+	private double playerCenterY = 0.0;
+	private Rotate playerRotate;
+	
+	private double mouseX = 0.0;
+	private double mouseY = 0.0;
 	
 	public static void main(String[] args)
 	{
@@ -35,14 +63,139 @@ public class RotationTest extends Application
 		window.setTitle("BubbleShot");
 		
 		root = new Group();
-		rec = new Rectangle(50, 100);
-		root.getChildren().add(rec);
+		mapGrid = new GridPane();
+		Room room = new Room();
+		Tile[][] roomTiles = room.getTiles();
 		
-		rec.getTransforms().add(new Translate(100,100));
-		rec.getTransforms().add(new Rotate(90));
+		for(int i = 0; i < roomTiles.length; i++)
+			for(int j = 0; j < roomTiles[0].length; j++)
+				mapGrid.add(roomTiles[i][j].getImageView(), j, i);
 		
-		scene = new Scene(root, 500,500);
+		moveArea = new Group();
+		Shape rec = new Rectangle(70, 10);
+		Shape cir = 
+		//player = Shape.union(cir, rec);
+		player = new Rectangle(70, 10);
+		//player = new Circle(25);
+		
+		playerCenterX = player.getBoundsInParent().getWidth() / 2;
+		playerCenterY = player.getBoundsInParent().getHeight() / 2;
+		
+		moveArea.getChildren().add(player);
+		
+		AnimationTimer animation = new AnimationTimer()
+		{
+			@Override
+			public void handle(long systime)
+			{
+				caculateMovement();
+				caculateMouseAngleToPlayer();
+				repositionPlayer();
+			}
+		};
+		animation.start();
+		
+		
+		headsUpDis = new BorderPane();
+		Text health = new Text("Health: 10");
+		health.setFont(new Font(30));
+		health.setStyle("fx-background-color : #FFFFFF");
+		headsUpDis.setTop(health);
+		
+		root.getChildren().add(mapGrid);
+		root.getChildren().add(moveArea);
+		root.getChildren().add(headsUpDis);
+		
+		
+		
+		scene = new Scene(root);
+		applyKeyEvents(scene);
 		window.setScene(scene);
 		window.show();
+	}
+	
+	private void caculateMovement() 
+	{
+		double deltaX = 0;
+		double deltaY = 0;
+		if(left)
+			deltaX -= 2;
+		if(right)
+			deltaX += 2;
+		if(up)
+			deltaY -= 2;
+		if(down)
+			deltaY += 2;
+		if(shift)
+		{
+			deltaX *= 5;
+			deltaY *= 5;
+		}
+		playerX += deltaX;
+		playerY += deltaY;
+		playerCenterX += deltaX;
+		playerCenterY += deltaY;
+	}
+	
+	private void repositionPlayer()
+	{
+		player.getTransforms().clear();
+		player.getTransforms().add(new Translate(playerX, playerY));
+		playerRotate = new Rotate(mouseAngle, player.getBoundsInLocal().getWidth()/2, player.getBoundsInLocal().getHeight()/2);
+		player.getTransforms().add(playerRotate);
+	}
+	
+	private void caculateMouseAngleToPlayer()
+	{
+		
+		//System.out.println("playerX: " + playerX);
+		//System.out.println("playerY: " + playerY);
+		
+		//System.out.println("playerCenterX: " + playerCenterX);
+		//System.out.println("playerCenterY: " + playerCenterY);
+		
+		//System.out.println("mouseAngle: " + mouseAngle);
+		
+		double distanceX = mouseX - playerCenterX;
+		double distanceY = mouseY - playerCenterY;
+		//System.out.println(distanceX);
+		//System.out.println(distanceY);
+		
+		mouseAngle = Math.toDegrees(Math.atan(distanceY / distanceX));
+		if(distanceY <= 0 && distanceX < 0)
+			mouseAngle += 180;
+		if(distanceY > 0 && distanceX < 0)
+			mouseAngle = 90 + (90 - Math.abs(mouseAngle));
+	}
+	
+	private void applyKeyEvents(Scene scene)
+	{
+		scene.setOnKeyPressed(event -> 
+		{
+			switch(event.getCode())
+			{
+				case W: up = true; break;
+				case A: left = true; break;
+				case S: down = true; break;
+				case D: right = true; break;
+				case SHIFT: shift = true; break;
+			}
+		});
+		scene.setOnKeyReleased(event -> 
+		{
+			switch(event.getCode())
+			{
+				case W: up = false; break;
+				case A: left = false; break;
+				case S: down = false; break;
+				case D: right = false; break;
+				case SHIFT: shift = false; break;
+			}
+		});
+		scene.addEventHandler(MouseEvent.ANY, event -> 
+		{
+			mouseX = event.getX();
+			mouseY = event.getY();
+		});
 	}
 }
