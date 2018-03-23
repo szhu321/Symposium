15719f1e3719e7870ee512dafbd395f8e07f1653
Student 3 <Student 3@10.8.33.141>
2018-03-23 10:16:13 -0400

    Item updates

--- BubbleShot/src/sprite/item/potion/DamagePot.java
+++ BubbleShot/src/sprite/item/potion/DamagePot.java
@@ -1,5 +1,21 @@
 package sprite.item.potion;
 
-public class DamagePot {
+import sprite.item.Consumable;
 
+public class DamagePot extends Potion implements Consumable
+{
+
+	DamagePot(String fileName, int xLocation, int yLocation, String itemType, String color, 
+			String effect) 
+	{
+		super(fileName, xLocation, yLocation, itemType, color, effect);
+	}
+
+	@Override
+	public boolean useItem() 
+	{
+		return false;
+	}
+	
+	
 }
