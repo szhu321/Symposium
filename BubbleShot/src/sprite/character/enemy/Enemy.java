00cf3953bea4145bd84651787ffa6c8c7784fd42
tobri <tobri@DESKTOP-CS6FU9C.fios-router.home>
2018-03-30 18:40:22 -0400

    killPoints

--- BubbleShot/src/sprite/character/enemy/Enemy.java
+++ BubbleShot/src/sprite/character/enemy/Enemy.java
@@ -5,11 +5,22 @@ import sprite.character.Character;
 abstract class Enemy extends Character
 {
 	private String weapon;
+	private int killPoints;
 	public Enemy(String spriteName,String fileName, double xLocation, double yLocation, int health, int speed,
 			String weapon) 
 	{
 		super(spriteName,fileName, xLocation, yLocation, health, speed,60,60);
 		this.weapon=weapon;
+	}
+	
+	public int getKillPoints()
+	{
+		return killPoints;
+	}
+	
+	public void setKillPoints(int killPoints)
+	{
+		this.killPoints=killPoints;
 	}
 	@Override
 	public void useSelectedItem(String input) 
