656bffea8f4420ae18eb2dd59ca26cfabfebb3a7
Sheng <Sheng@Shengs-PC>
2018-04-02 19:26:37 -0400

    Beginning to connect back to front end

--- BubbleShot/src/mainGame/backend/GameManager.java
+++ BubbleShot/src/mainGame/backend/GameManager.java
@@ -1,10 +1,20 @@
 package mainGame.backend;
 
+import map.Level;
+import sprite.character.player.Player;
+
 public class GameManager
 {
+	private Level level;
+	private Player player;
 	
+	public GameManager(Level level, Player player)
+	{
+		this.level = level;
+		this.player = player;
+	}
 	
-	public GameManager()
+	public void startGame()
 	{
 		
 	}
