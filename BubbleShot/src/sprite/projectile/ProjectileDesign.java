a7b785b35d8c522c6e7a43fcf5b591da1cec06cf
Sheng <Sheng@Shengs-PC>
2018-04-14 17:39:10 -0400

    minor Change

--- BubbleShot/src/sprite/projectile/ProjectileDesign.java
+++ BubbleShot/src/sprite/projectile/ProjectileDesign.java
@@ -5,8 +5,8 @@ public class ProjectileDesign
 	public static Projectile getBulletDesignOne(String bulletOwner, double xLocation, double yLocation, double faceAngle, int damage)
 	{
 		String fileName = "file:resources/projectilePictures/bullet.png";
-		double width = 100;
-		double height = 20;
+		double width = 10;
+		double height = 10;
 		Projectile bullet = new LinearProjectile("Simple Bullet", fileName, bulletOwner, xLocation, yLocation, width, height, 500, faceAngle, damage);
 		return bullet;
 	}
