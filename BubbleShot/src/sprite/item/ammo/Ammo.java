385e5d3c389e5e4654991cba92e3ba0955a4b960
Student 3 <Student 3@10.8.33.173>
2018-04-23 10:26:54 -0400

    Ammo class update

--- BubbleShot/src/sprite/item/ammo/Ammo.java
+++ BubbleShot/src/sprite/item/ammo/Ammo.java
@@ -1,5 +1,23 @@
 package sprite.item.ammo;
 
-public class Ammo {
+import sprite.item.Item;
 
+public class Ammo extends Item
+{
+	int currentAmmo = 35;
+	
+	public Ammo(String spriteName, String fileName, double xLocation, double yLocation, 
+			String itemType, boolean isCooledDown, double coolDownTime, double width, 
+			double height) 
+	{
+		super(spriteName, fileName, xLocation, yLocation, itemType, isCooledDown, coolDownTime, 
+				width, height);
+	}
+
+	@Override
+	public void useItem() 
+	{
+		currentAmmo--;
+	}
+	
 }
