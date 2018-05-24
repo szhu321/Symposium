050bc7e6e762d0719431885949d3628a633f01ce
Sheng <Sheng@Shengs-PC>
2018-05-23 20:14:42 -0400

    coin update

--- BubbleShot/src/sprite/item/collectable/CoinDesign.java
+++ BubbleShot/src/sprite/item/collectable/CoinDesign.java
@@ -3,6 +3,8 @@ package sprite.item.collectable;
 import java.util.ArrayList;
 import java.util.List;
 
+import sprite.item.Item;
+
 public class CoinDesign
 {
 	public static Coin getCoinOne(double x, double y)
@@ -15,9 +17,9 @@ public class CoinDesign
 		return new Coin("", "file:resources/coin/coin5.png", x, y, 30, 30, 5);
 	}
 	
-	public static List<Coin> getCoinStack(int amount)
+	public static List<Item> getCoinStack(int amount)
 	{
-		List<Coin> coins = new ArrayList<Coin>();
+		List<Item> coins = new ArrayList<Item>();
 		int amountleft = amount;
 		while(amountleft >= 5)
 		{
