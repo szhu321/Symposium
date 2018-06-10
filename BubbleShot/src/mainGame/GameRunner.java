a1f26e7dba2f987e4be8da88122c694c9fa28054
Sheng <Sheng@Shengs-PC>
2018-06-10 16:20:44 -0400

    new player

--- BubbleShot/src/mainGame/GameRunner.java
+++ BubbleShot/src/mainGame/GameRunner.java
@@ -34,15 +34,15 @@ public class GameRunner extends Application
 		window = primaryStage;
 		window.setTitle("Lost Cause");
 		
-		Player player = PlayerDesign.getSimpleStarterPlayer("Joy");
-//		//Enemy[] enemyList= {EnesmyDesign.getRegularDesignOne(500, 500,player),EnemyDesign.getRegularDesignOne(500, 600,player)};
-		gameManager = new GameManager(LevelDesign.getLevelTestBoss(), player, window);
-		gameManager.startGame();
+//		Player player = PlayerDesign.getSimpleStarterPlayer("Joy");
+////		//Enemy[] enemyList= {EnesmyDesign.getRegularDesignOne(500, 500,player),EnemyDesign.getRegularDesignOne(500, 600,player)};
+//		gameManager = new GameManager(LevelDesign.getLevelTestBoss(), player, window);
+//		gameManager.startGame();
 		
 //		BackgroundSound bs = new BackgroundSound("resources/music/AlanWForce.mp3", 232);
 //		bs.playSound(.05);
 //		
-		//loadFXMLs();
+		loadFXMLs();
 		
 		
 		
