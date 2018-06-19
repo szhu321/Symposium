cb11b65370e7a6fcff303853ed65589cf5e4c6a6
Sheng <Sheng@Shengs-PC>
2018-06-19 13:17:44 -0400

    memory sound fix

--- BubbleShot/src/sound/EffectSound.java
+++ BubbleShot/src/sound/EffectSound.java
@@ -1,33 +1,55 @@
 package sound;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
-public class EffectSound implements Sound
+public class EffectSound implements Sound, Runnable
 {
 	private Media effectSound;
 	private MediaPlayer mediaPlayer;
+	private String soundFile;
+	private List<MediaPlayer> players;
+	private double duration;
 	//in seconds
 	//private double effectSoundLength;
 	
-	public EffectSound(String soundFile)
+	public EffectSound(String soundFile, double duration)
 	{
+		this.soundFile = soundFile;
 		effectSound = new Media(new File(soundFile).toURI().toString());
-		//effectSoundLength = eSLength;
+		mediaPlayer = new MediaPlayer(effectSound);
+		players = new ArrayList<MediaPlayer>();
+		this.duration = duration;
 	}
 
 	@Override
 	public void playSound() 
 	{
-		mediaPlayer = new MediaPlayer(effectSound);
+		
 		//mediaPlayer.setVolume(volume);
 		//mediaPlayer.setStartTime(Duration.seconds(0));
-		//mediaPlayer.setStopTime(Duration.seconds(effectSoundLength));
+		
 		//mediaPlayer.setAutoPlay(true);
+		//stopSound();
+		mediaPlayer = new MediaPlayer(effectSound);
+		players.add(mediaPlayer);
+		mediaPlayer.setStopTime(Duration.seconds(duration));
+		mediaPlayer.setOnEndOfMedia(this);
 		mediaPlayer.play();
+		//mediaPlayer.d
+		
+	}
+	
+	@Override
+	public void run()
+	{
+		players.remove(0).dispose();
+		//System.out.println(players.size());
 	}
 
 	@Override
@@ -35,4 +57,6 @@ public class EffectSound implements Sound
 	{
 		mediaPlayer.stop();
 	}
+
+	
 }
--- BubbleShot/src/sound/SoundEffects.java
+++ BubbleShot/src/sound/SoundEffects.java
@@ -2,5 +2,6 @@ package sound;
 
 public class SoundEffects
 {
-	public static final EffectSound GUN_SHOT_SOUND = new EffectSound("resources/soundEffects/gunShot.wav");
+	public static final EffectSound GUN_SHOT_SOUND = new EffectSound("resources/soundEffects/gunShot.wav", .5);
+	public static final EffectSound PEW_PEW_SOUND = new EffectSound("resources/soundEffects/pew_pew.mp3", .5);
 }
